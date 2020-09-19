60cdf1cdb8b437a35faaeca64028a43e6cb9a4b0
Jean-Philippe Bidegain <jp@bidega.in>
2020-09-19 12:45:52 +0200

    Add a proper structure to an album

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,63 @@ use clap::App;
 extern crate id3;
 extern crate mime_guess;
 
+extern crate clogger;
+
+#[macro_use]
+extern crate log;
+
 use id3::Tag;
+use std::fmt;
 use std::fs;
 use std::path::Path;
 use std::path::PathBuf;
+
+
+#[derive(Debug)]
+pub struct Album {
+    pub artist: String,
+    pub year: String,
+    pub name: String,
+}
+
+impl Album {
+    pub fn new(artist: String, year: String, name: String) -> Self {
+        Album {
+            artist,
+            year,
+            name,
+        }
+    }
+
+    pub fn from_tag(tag: Tag) -> Option<Album> {
+        let artist = match tag.artist() {
+            Some(artist) => artist.to_owned(),
+            None => return None,
+        };
+
+        let year = match tag.year() {
+            Some(year) => year.to_string().to_owned(),
+            None => return None,
+        };
+
+        let name = match tag.album() {
+            Some(name) => name.to_owned(),
+            None => return None,
+        };
+
+        Some(Album {
+            artist,
+            year,
+            name,
+        })
+    }
+}
+
+impl fmt::Display for Album {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{} - {} - {}", self.artist, self.year, self.name)
+    }
+}
 
 fn get_tag_from_filepath(path: &str) -> id3::Tag {
     id3::Tag::read_from_path(path).unwrap()
@@ -20,20 +73,28 @@ fn is_mp3(file_name: &str) -> bool {
 }
 
 fn main() {
+    clogger::init();
+
     // Load options from cli arguments
     let yaml = load_yaml!("cli.yml");
     let matches = App::from_yaml(yaml).get_matches();
 
+    // Debug?
+    if matches.is_present("debug") {
+        clogger::set_verbosity(2);
+    }
+
     // Dry-run mode?
     let dry_run = matches.is_present("dry-run");
     if dry_run == true {
-        println!("Running in dry-run mode");
+        debug!("Running in dry-run mode");
     }
 
-    // Parsing all the folders in arguments
+    debug!("Parsing all the folders in arguments");
+
     if let Some(folders) = matches.values_of("SRC") {
         for folder in folders {
-            println!("Parsing folder: {}", folder);
+            debug!("Parsing folder: {}", folder);
 
             // Parse the folder to find a mp3 file
             let paths = fs::read_dir(folder).unwrap();
@@ -44,42 +105,37 @@ fn main() {
                 let file_name = entry_path.file_name().unwrap().to_str().unwrap();
 
                 if is_mp3(file_name) {
-                    println!("MP3 found {:?}", file_name);
+                    debug!("MP3 found {:?}", file_name);
 
                     // Building path
                     let mut full_path = PathBuf::from(folder.to_string());
                     full_path.push(file_name);
 
-                    // Getting the tags
+                    debug!("Getting the tags from {}", file_name);
                     let tag: Tag = get_tag_from_filepath(full_path.to_str().unwrap());
 
-                    // Building the folder name
-                    let mut new_folder_name: String = "".to_owned();
+                    if let Some(album) = Album::from_tag(tag) {
+                        info!("All information found in {}", file_name);
+                        debug!("Artist: {} / Year: {} / Album: {}", album.artist, album.year, album.name);
 
-                    let artist = tag.artist().unwrap().to_string();
-                    new_folder_name.push_str(&artist);
+                        debug!("Building the folder name");
+                        let new_folder_name: String = album.to_string();
 
-                    let year = tag.year().unwrap().to_string();
-                    new_folder_name.push_str(" - ");
-                    new_folder_name.push_str(&year);
+                        let old_path = Path::new(&folder);
+                        let parent = old_path.parent().unwrap();
+                        let new_path = parent.join(new_folder_name);
 
-                    let album = tag.album().unwrap().to_owned();
-                    new_folder_name.push_str(" - ");
-                    new_folder_name.push_str(&album);
+                        info!("Renaming folder from {:?} to {:?}", old_path, new_path);
 
-                    let old_path = Path::new(&folder);
-                    let parent = old_path.parent().unwrap();
-                    let new_path = parent.join(new_folder_name);
+                        if dry_run == false {
+                            let res = fs::rename(folder, new_path);
+                            println!("Result: {:?}", res);
+                        }
 
-                    println!("New from {:?} to {:?}", old_path, new_path);
-
-                    // Renaming the folder
-                    if dry_run == false {
-                        let res = fs::rename(folder, new_path);
-                        println!("Result: {:?}", res);
+                        break;
+                    } else {
+                        warn!("Not all information where found if \"{}\" tags", file_name);
                     }
-
-                    break;
                 }
             }
         }
